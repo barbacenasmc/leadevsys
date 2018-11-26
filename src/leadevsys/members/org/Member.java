edb712eef9bcba016815dfac1c577f9a81b4556c
Shiela Marie Barbacena <shiela.barbacena@leafnode.io>
2018-11-26 18:05:49 +0800

    added new details to member entity class

--- src/leadevsys/members/org/Member.java
+++ src/leadevsys/members/org/Member.java
@@ -5,7 +5,78 @@ public class Member {
     private String fname;
     private String mname;
     private String lname;
+    private String memberLocation;
+    private String gender;
+    private String civilStatus;
+    private String contactNo;
+    private String birthdate;
+    private String school;
+    private String occupation;
 
+    public int getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(int member_id) {
+        this.member_id = member_id;
+    }
+
+    public String getMemberLocation() {
+        return memberLocation;
+    }
+
+    public void setMemberLocation(String memberLocation) {
+        this.memberLocation = memberLocation;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCivilStatus() {
+        return civilStatus;
+    }
+
+    public void setCivilStatus(String civilStatus) {
+        this.civilStatus = civilStatus;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+    
     
     public int getMemberId(){
         return member_id;
